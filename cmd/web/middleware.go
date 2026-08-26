02e7ddf5ff10c761467bab11806c252499d231aa
son truong <sntruong12@gmail.com>
2026-08-26 17:59:56 -0500

    log various request meta data

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -32,3 +32,11 @@ func secureHeaders(next http.Handler) http.Handler {
 		// code here is executed after being returned from the next handler
 	})
 }
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,5 +15,5 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/ride/view", app.rideView)
 	mux.HandleFunc("/ride/create", app.rideCreate)
 
-	return secureHeaders(mux)
+	return app.logRequest(secureHeaders(mux))
 }
