16d0a973a0b8939d7151015ff21e349f0cd1d4ab
son truong <sntruong12@gmail.com>
2026-09-04 23:05:59 -0500

    initial scaffold for log in/out/signup

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,13 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /ride/create", dynamic.ThenFunc(app.rideCreate))
 	mux.Handle("POST /ride/create", dynamic.ThenFunc(app.rideCreatePost))
 
+	// Add the five new routes, all of which use our 'dynamic' middleware chain.
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
 	// Create a middleware chain containing our 'standard' middleware
 	// which will be used for every request our application receives.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
