e9c3d7b43b8851b6a93d77e27702f394a9f8dee2
son truong <sntruong12@gmail.com>
2026-09-09 22:29:08 -0500

    use go context to persist/check for authenticated user in route handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,7 +16,7 @@ func (app *application) routes() http.Handler {
 	// Create a new middleware chain containing the middleware specific to our
 	// dynamic application routes. For now, this chain will only contain the
 	// LoadAndSave session middleware but we'll add more to it later.
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	// Unprotected application routes using the "dynamic" middleware chain.
 	mux.Handle("GET /", dynamic.ThenFunc(app.home))
