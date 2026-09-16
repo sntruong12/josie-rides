5fe0bbfc4942dc626c494cf94c39d34a971319c4
son truong <sntruong12@gmail.com>
2026-09-16 15:46:21 -0500

    added testing for GET create ride view

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -79,6 +79,39 @@ func TestRideView(t *testing.T) {
 	}
 }
 
+func TestRideCreateView(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/ride/create")
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		// Log in by posting valid credentials to /user/login. First, get the
+		// login page to extract a valid CSRF token.
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "pa$$word")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+
+		// Now the session has authenticatedUserID set, so the protected
+		// route should be accessible.
+		code, _, body := ts.get(t, "/ride/create")
+
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, `<form action="/ride/create" method="POST"`)
+	})
+}
+
 func TestUserSignup(t *testing.T) {
 	app := newTestApplication(t)
 	ts := newTestServer(t, app.routes())
